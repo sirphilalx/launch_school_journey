2959a6c1ad6dc5fc81f786342f681534a2f01ffe
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-07-12 12:21:16 +0100

    odd numbers

---
+++ smallProblems/oddNumbers.js
@@ -0,0 +1,10 @@
+/*
+
+Log all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+*/
+
+for (let i = 0; i < 100; i++){
+	if (i % 2 !== 0)
+		console.log(i);
+}
